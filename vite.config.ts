0d3094e5f2cf4707d8f42bdce1bb40fd4b2aa844
vardhini330 <vardhinis57@gmail.com>
2026-03-02 13:35:27 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/vit-smartinfra/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
